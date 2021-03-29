10e19e01059638b1251e8f69a28ab294f9a52dea
dao_bui_vn <dao.bui@evizi.com>
2021-03-29 16:07:38 +0700

    inittial project

---
+++ src/main/java/edu/springframework/zbreweryservice/ZbreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.springframework.zbreweryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbreweryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbreweryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/springframework/zbreweryservice/ZbreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.springframework.zbreweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbreweryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
